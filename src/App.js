295e12e24ebc6d4ee13f86d4ccccd5a7adcd29fe
Patty Fresonke <pattyfresonke@gmail.com>
2018-10-03 22:42:53 -0500

    adding functionality

--- src/App.js
+++ src/App.js
@@ -1,28 +1,125 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 class App extends Component {
+
+  state = {
+    venues: []
+  }
+
+  componentDidMount() {
+    this.getVenues()
+  }
+
+  renderMap = () => {
+    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyBNiVrOQqLRb6SuJgjBRM_bQV2hYAs-hRw&callback=initMap")
+    window.initMap = this.initMap
+  }
+
+  getVenues = () => {
+    const endPoint = "https://api.foursquare.com/v2/venues/explore?"
+    const parameters = {
+      client_id: "ORMEUFMFTJIPPYRESL0CA30M1X0QNGU1NRZ03NKD311S4QDH",
+      client_secret: "RN4OH1PHIAZAMH52FHZ5I4LGA5VULXXGQ304C00ALCZXCKNK",
+      v: "20181003",
+      query: "shopping",
+      near: "Detroit, MI"
+    }
+
+    axios.get(endPoint + new URLSearchParams(parameters))
+      .then(response => {
+        this.setState({
+          venues: response.data.response.groups[0].items
+        }, this.renderMap())
+      })
+      .catch(error => {
+        console.log("Error: " + error)
+      })
+  }
+
+
+  initMap = () => {
+    // Create a map
+    var map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: 42.3823699, lng: -83.0793885},
+      zoom: 11
+    })
+
+    var infoWindow = new window.google.maps.InfoWindow()
+
+    // Parse through results
+    this.state.venues.map(v => {
+      var venueName = `<p>${v.venue.name}</p> <p>${v.venue.location.address}</p>`
+
+      // Create marker for each result
+      var marker = new window.google.maps.Marker({
+        position: {lat: v.venue.location.lat, lng: v.venue.location.lng},
+        map: map,
+        title: v.venue.name,
+        animation: window.google.maps.Animation.DROP
+      })
+
+      // Listen for a click on a marker
+      // Set content based on each marker
+      // Display content in an info window
+      marker.addListener('click', function() {
+        infoWindow.setContent(venueName)
+        infoWindow.open(map, marker)
+      })
+    })
+
+    var venueList = new window.document.createElement("ul")
+
+    // Parse through results
+    this.state.venues.map(v => {
+      var venueName = `<p>${v.venue.name}</p> <p>${v.venue.location.address}</p>`
+
+      // Create marker for each result
+      var listItem = new window.google.maps.Marker({
+        position: {lat: v.venue.location.lat, lng: v.venue.location.lng},
+        map: map,
+        title: v.venue.name,
+        animation: window.google.maps.Animation.DROP
+      })
+
+      // Listen for a click on a marker
+      // Set content based on each marker
+      // Display content in an info window
+      marker.addListener('click', function() {
+        infoWindow.setContent(venueName)
+        infoWindow.open(map, marker)
+      })
+    })
+
+
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div class="container">
+        <div class="sidebar">
+          <h1>Neighborhood Map</h1>
+          <div id="list">
+            <input id="show-listings" type="button" value="Show Listings" />
+            <input id="hide-listings" type="button" value="Hide Listings" />
+          </div>
+        </div>
+
+        <div id="map"></div>
       </div>
-    );
+    )
   }
+}
+
+function loadScript(url) {
+  var index = window.document.getElementsByTagName("script")[0]
+  var script = window.document.createElement("script")
+  script.src = url
+  script.async = true
+  script.defer = true
+  index.parentNode.insertBefore(script, index)
 }
 
 export default App;
