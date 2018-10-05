993a734c3a1e221bfc81856e98b3936e7584474b
Patty Fresonke <pattyfresonke@gmail.com>
2018-10-04 22:05:42 -0500

    adding default/label option to dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -6,6 +6,7 @@ class Dropdown extends Component {
     return (
       <div>
         <select className='filter'>
+          <option disabled selected value="Filter by Category">--Filter by Category--</option>
           {this.props.venues && 
             this.props.venues.map((venue, referralId) => (
               <DropdownItem key={referralId} {...venue}/>
